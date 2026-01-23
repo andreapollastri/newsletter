d590d8ebf27c0712269e5a50bcc1b55bebdd0e1e
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2026-01-23 11:24:23 +0100

    Revert "Fix"
    This reverts commit c95ab3e4c609063a791528c6b734fc8235b241ae.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,7 @@ class AppServiceProvider extends ServiceProvider
         }
 
         // Ensure Livewire temporary upload directory exists
-        // Livewire uses the default filesystem disk, which is 'local' pointing to storage/app/private
-        $defaultDisk = config('filesystems.default', 'local');
-        $diskRoot = config("filesystems.disks.{$defaultDisk}.root", storage_path('app'));
-        
-        $livewireTmpPath = $diskRoot.'/livewire-tmp';
+        $livewireTmpPath = storage_path('app/livewire-tmp');
         if (! is_dir($livewireTmpPath)) {
             mkdir($livewireTmpPath, 0755, true);
         }
